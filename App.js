b23d5745fd1558f9baf304eb577aa8e8c562bb62
Rajesh Kumar P <rajeshkumar.p9025@gmail.com>
2025-02-10 23:15:57 +0530

    feat(food-app-ep-03): added base components header | body | res-card

---
+++ App.js
@@ -0,0 +1,59 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurentCard = () => {
+  return (
+    <div className="res-card">
+      <img alt="res-img" className="res-img" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/iivuhjc2mswi9lublktf" />
+      <h4>Megahana Food</h4>
+      <h3>Briyani, North Indian, South Indian</h3>
+      <h3>4.4 stars</h3>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">search</div>
+      <div className="res-container">
+        <RestaurentCard />
+      </div>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+export default App;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Food-Ordering-App
+
+# part-4 talk is cheap show THE code
+
+-  planning
+-food ordering app
+-config driven ui
